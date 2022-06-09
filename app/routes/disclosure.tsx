f248b4b485d4cdc3528351f1995a3bd2bedab3ff
Ian Duvall <iduvall@umich.edu>
2022-06-09 07:55:00 -0500

    Add progressive disclosure

--- app/routes/disclosure.tsx
+++ app/routes/disclosure.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 
+const id = "disclosure";
+const isClient = () => typeof document !== "undefined";
+
+const openStateInit = () => {
+  if (isClient()) {
+    const disclosureElement = document.getElementById(
+      id
+    ) as HTMLDetailsElement | null;
+
+    return Boolean(disclosureElement?.open);
+  }
+
+  return false;
+};
+
 export default function DisclosureRoute() {
   console.log("rendering");
 
-  const [open, setOpen] = React.useState<boolean>(false);
+  const [open, setOpen] = React.useState<boolean>(openStateInit);
 
   return (
     <details
+      id={id}
       open={open}
-      onToggle={() => {
+      onToggle={(ev) => {
         console.log("toggling");
-        setOpen((prev) => !prev);
+        setOpen(ev.currentTarget.open);
       }}
     >
       <summary>Summary</summary>
